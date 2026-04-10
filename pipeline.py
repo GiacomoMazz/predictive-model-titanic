131649b65622a86bb2294bc9ef36e678029dab90
Giacomo Mazzarella <twooyeezy@gmail.com>
2026-04-10 20:56:10 +0200

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 from utils.data_management.data_analysis import *
 from utils.data_management.metric_calculator import *
+from utils.data_management.Training import *
 
 import pandas as pd
 
@@ -14,4 +15,23 @@ if __name__ == '__main__':
 
     # removing redundant columns
 
+
+    remove_columns(df, ["PassengerId", "Cabin", "Ticket", "Name"])
+
+    df = pd.get_dummies(df, columns = ["Sex", "Embarked"], dtype = int)
+
     run_eda(df, "Survived")
+
+    split_tuple = split_data(df)
+
+    train_data = split_tuple[0]
+    val_data = split_tuple[1]
+    test_data = split_tuple[2]
+
+    # data preperation using dummy variables
+
+
+    build_decision_tree(train_data)
+
+    
+
